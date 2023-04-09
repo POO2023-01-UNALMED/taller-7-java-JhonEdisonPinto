668b9d0471ba6d5080f43b9c0bf750a259245b31
JhonEdisonPinto <124928317+JhonEdisonPinto@users.noreply.github.com>
2023-04-09 12:06:50 -0500

    Primer intento

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,49 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma {
+	private String[] letras;
+	private String interpretacion;
+	
+
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		this.letras=letras;
+		this.interpretacion= interpretacion;
+	}
+	
+	public String[] getLetras() {
+		return letras;
+	}
+	public void setLetras(String[] letras) {
+		this.letras=letras;
+	}
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion=interpretacion;
+	}
+	
+	//Metodo cantidad de letras
+	public int cantidadLetras() {
+		int cantidadL=getLetras().length;
+		return cantidadL;
+	}
+	//Interpretacion
+	@Override
+	public String interpretacion() {
+		return interpretacion;
+	}
+	@Override 
+	public String toString(){
+		String cadaLetra ="";
+		for(int i = 0; i<=letras.length;i++) {
+			cadaLetra += letras[i];
+			if (i<letras.length-1) {
+				cadaLetra += ",";
+			}
+		}
+	return cadaLetra;
+	}
+}
+
---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,48 @@
+package comunicacion;
+
+public class Fabula extends Escrito{
+	String ensenanza;
+	String interpretacion;
+	
+	public Fabula(String origen, String titulo, String autor, int paginas,
+			String ensenanza, String interpretacion) {
+		super(origen,titulo,autor,paginas);
+		this.ensenanza=ensenanza;
+		this.interpretacion=interpretacion;
+	}
+	
+	public String getEnsenanza() {
+		return ensenanza;
+	}
+	
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza=ensenanza;
+	}
+	
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion=interpretacion;
+	}
+	@Override
+	public int palabrasTotales (int palabraspag) {
+		return palabraspag*getPaginas();
+		
+	}
+	@Override
+	public String interpretacion() {
+		return interpretacion;
+	}
+	
+	@Override
+	public String toString() {
+		return
+		getOrigen()+"\n"+
+		getTitulo()+"\n"+
+		getAutor()+"\n"+
+		getPaginas()+"\n"+
+		getEnsenanza();
+	}
+
+}
